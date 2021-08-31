15126b1d75e0506c49adb11058fcafdb495a9030
Betuntun <36393621+Betuntun@users.noreply.github.com>
2021-08-31 15:50:50 -0600

    Mejorando la app

--- src/app/pais/pages/por-region/por-region.component.ts
+++ src/app/pais/pages/por-region/por-region.component.ts
@@ -1,16 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import { Country } from '../../interfaces/pais.interface';
+import { PaisService } from '../../services/pais.service';
+
 
 @Component({
   selector: 'app-por-region',
   templateUrl: './por-region.component.html',
   styles: [
+    `
+    button{
+      margin-right:5px;
+    }
+    `
   ]
 })
 export class PorRegionComponent implements OnInit {
 
-  constructor() { }
+  regiones: string[] = ['Africa', 'Americas', 'Asia', 'Europe', 'Oceania'];;
+  regionActiva:string = '';
+  paises : Country[] = [];
+  constructor( private paisService: PaisService) { }
 
   ngOnInit(): void {
   }
 
+  activarRegion(region:string){
+    if(region == this.regionActiva){return}
+    this.regionActiva = region;
+    this.paises = [];
+    this.paisService.buscarRegion(region)
+    .subscribe( resp => {
+      this.paises = resp;
+    });
+  }
+
+  getClaseCss(region : string){
+    return  region === this.regionActiva ? 'btn btn-primary': 'btn btn-outline-primary' ;
+  }
 }
